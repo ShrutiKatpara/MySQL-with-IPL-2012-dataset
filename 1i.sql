e6f8a973320ba9981869bdb1ea657aa71f74fb1d
Shruti Katpara <45987734+ShrutiKatpara@users.noreply.github.com>
2021-02-15 18:44:59 +0530

    1847

--- 1i.sql
+++ 1i.sql
@@ -1,12 +1,21 @@
-create view indian_players as 
-select player_id, player_name , sum(runs_scored)
-from ball_by_ball
-natural join batsman_scored
-inner join player on player.player_id = ball_by_ball.striker and player.country_name = "India"
-group by striker
-order by player_name
+create view onei as
+select player.player_name, player.country_name , sum( coalesce(runs,0)) as total_runs
+from player
+left join 
+(select ball_by_ball.striker as id, batsman_scored.runs_scored as runs
+from ball_by_ball, batsman_scored
+where ball_by_ball.match_id = batsman_scored.match_id and ball_by_ball.over_id = batsman_scored.over_id and ball_by_ball.ball_id = batsman_scored.ball_id and ball_by_ball.innings_no = batsman_scored.innings_no
+) as temp on temp.id = player.player_id and player.country_name = "India"
+group by player.player_id
+;
+
+create view indian_players as
+select player_name, total_runs
+from onei
+where country_name = "India"
 ;
 
 select * from indian_players;
 
+drop view onei;
 drop view indian_players;
